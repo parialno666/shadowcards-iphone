81f355a3b24266593fdf90737949c66c83defccf
parialno666 <parialno666@users.noreply.github.com>
2026-07-25 19:56:16 +0300

    Allow importing Shadow files on iPhone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shadowcards-static-v2";
+const CACHE = "shadowcards-static-v3";
 const FILES = ["./", "./index.html", "./app.js", "./styles.css", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
